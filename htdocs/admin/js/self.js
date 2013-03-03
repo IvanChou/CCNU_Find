ed48558b1d74ed98c36837f172bf0f3abc0c3060
xworm <xworm@xworm.net>
2013-03-03 20:48:38 +0800

    admin table & list

--- htdocs/admin/js/self.js
+++ htdocs/admin/js/self.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
     $("#contact, #home, #manage").css('min-height',$(window).height());
+
+    $("head").append($('<style>.bs-docs-example:after { content: " 你可以不坑爹么？" }</style>'));
+
 });
